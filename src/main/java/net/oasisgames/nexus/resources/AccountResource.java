feabf2341e573a77d3ca5c409411fed1689ace8f
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-17 16:03:00 -0400

    Updated image upload endpoints

--- src/main/java/net/oasisgames/nexus/resources/AccountResource.java
+++ src/main/java/net/oasisgames/nexus/resources/AccountResource.java
@@ -1,15 +1,17 @@
 package net.oasisgames.nexus.resources;
 
 import lombok.RequiredArgsConstructor;
+import net.oasisgames.nexus.dto.ImageDto;
 import net.oasisgames.nexus.dto.UserDto;
 import net.oasisgames.nexus.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/account")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AccountResource {
 
     private final UserService userService;
@@ -27,6 +29,18 @@ public class AccountResource {
     @PatchMapping("/edit")
     public ResponseEntity<UserDto> edit(@RequestBody UserDto user) {
         return ResponseEntity.ok().body(userService.editExistingUser(user));
+    }
+
+    @PostMapping(value = "/{id}/set-profile-picture")
+    public ResponseEntity<UserDto> setProfilePicture(@PathVariable String id,
+                                                     @RequestParam("file") MultipartFile file) {
+        userService.saveProfilePicture(id, file);
+        return ResponseEntity.ok().body(userService.getUserById(id));
+    }
+
+    @GetMapping("/{id}/get-profile-picture")
+    public ResponseEntity<ImageDto> getProfilePicture(@PathVariable String id) {
+        return ResponseEntity.ok().body(userService.getProfilePicture(id));
     }
 
 }
